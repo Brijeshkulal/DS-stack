e8f29196faf691539d25d5804b9967f9cddf90bf
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-19 23:18:12 +0530

    Added push method to create a stack using linkedlist

--- src/com/ds/stack/Stack.java
+++ src/com/ds/stack/Stack.java
@@ -1,6 +1,23 @@
 package com.ds.stack;
 
 public class Stack {
+	Node top;
+	int size;
 	
+	public void push(int data) {
+		Node node = new Node(data);
+		node.next = top;
+		top = node;
+		size++;
+	}
+	
+	public void display()
+	{
+		Node temp = top;
+		while(temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
 
 }
--- src/com/ds/stack/StackMain.java
+++ src/com/ds/stack/StackMain.java
@@ -2,7 +2,11 @@ package com.ds.stack;
 
 public class StackMain {
 	public static void main(String[] args) {
-		System.out.println("welcome to stake programme");
+		Stack stack = new Stack();
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		stack.display();
 	}
 
 }
