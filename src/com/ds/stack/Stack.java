a3fee988c51c39e059f4a8f2e85b652a41dc9fa0
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-19 18:57:37 +0530

    Added welcome message

---
+++ src/com/ds/stack/Stack.java
@@ -0,0 +1,6 @@
+package com.ds.stack;
+
+public class Stack {
+	
+
+}
---
+++ src/com/ds/stack/StackMain.java
@@ -0,0 +1,8 @@
+package com.ds.stack;
+
+public class StackMain {
+	public static void main(String[] args) {
+		System.out.println("welcome to stake programme");
+	}
+
+}
